b8178d68f57f50229e354993a0c79150c33986e1
miraclesu <suchuangji@gmail.com>
2021-01-24 17:52:15 +0800

    entities: WETH token

--- entities/token.go
+++ entities/token.go
@@ -1,10 +1,48 @@
 package entities
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/miraclesu/uniswap-sdk-go/constants"
 	"github.com/miraclesu/uniswap-sdk-go/utils"
+)
+
+var (
+	ErrDiffChainID = fmt.Errorf("diff chain id")
+	ErrSameAddrss  = fmt.Errorf("same address")
+
+	_WETHCurrency, _ = newCurrency(18, "WETH", "Wrapped Ether")
+
+	WETH = map[constants.ChainID]*Token{
+		constants.Mainnet: {
+			Currency: _WETHCurrency,
+			ChainID:  constants.Mainnet,
+			Address:  utils.ValidateAndParseAddress("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"),
+		},
+		constants.Ropsten: {
+			Currency: _WETHCurrency,
+			ChainID:  constants.Ropsten,
+			Address:  utils.ValidateAndParseAddress("0xc778417E063141139Fce010982780140Aa0cD5Ab"),
+		},
+		constants.Rinkeby: {
+			Currency: _WETHCurrency,
+			ChainID:  constants.Rinkeby,
+			Address:  utils.ValidateAndParseAddress("0xc778417E063141139Fce010982780140Aa0cD5Ab"),
+		},
+		constants.Goerli: {
+			Currency: _WETHCurrency,
+			ChainID:  constants.Goerli,
+			Address:  utils.ValidateAndParseAddress("0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"),
+		},
+		constants.Kovan: {
+			Currency: _WETHCurrency,
+			ChainID:  constants.Kovan,
+			Address:  utils.ValidateAndParseAddress("0xd0A1E359811322d97991E03f863a0C30C2cF029C"),
+		},
+	}
 )
 
 /**
@@ -40,4 +78,21 @@ func (t *Token) Equals(other *Token) bool {
 	}
 
 	return t.ChainID == other.ChainID && t.Address == other.Address
+}
+
+/**
+ * Returns true if the address of this token sorts before the address of the other token
+ * @param other other token to compare
+ * @throws if the tokens have the same address
+ * @throws if the tokens are on different chains
+ */
+func (t *Token) SortsBefore(other *Token) (bool, error) {
+	if t.ChainID != other.ChainID {
+		return false, ErrDiffChainID
+	}
+	if t.Address == other.Address {
+		return false, ErrSameAddrss
+	}
+
+	return strings.ToLower(t.Address.String()) < strings.ToLower(other.Address.String()), nil
 }
