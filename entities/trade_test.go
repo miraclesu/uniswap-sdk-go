534645e6bf2e180ed04155a9b9a383e82f097789
miraclesu <suchuangji@gmail.com>
2021-03-27 17:57:49 +0800

    trade: add ETHER test cases
    warp as WETH first

--- entities/trade_test.go
+++ entities/trade_test.go
@@ -32,8 +32,9 @@ func TestTrade(t *testing.T) {
 	pair_1_3, _ := NewPair(tokenAmount_1_1200, tokenAmount_3_1300)
 
 	// use WETH as ETHR
-	tokenETHR := WETH[constants.Mainnet]
-	tokenAmount_0_weth, _ := NewTokenAmount(tokenETHR, big.NewInt(1000))
+	tokenETHER := WETH[constants.Mainnet]
+	tokenAmountETHER, _ := NewTokenAmount(tokenETHER, big.NewInt(100))
+	tokenAmount_0_weth, _ := NewTokenAmount(tokenETHER, big.NewInt(1000))
 	pair_weth_0, _ := NewPair(tokenAmount_0_weth, tokenAmount_0_1000)
 
 	tokenAmount_0_0, _ := NewTokenAmount(token0, big.NewInt(0))
@@ -42,13 +43,12 @@ func TestTrade(t *testing.T) {
 	_ = empty_pair_0_1
 
 	{
-		route, _ := NewRoute([]*Pair{pair_weth_0}, tokenETHR, nil)
-		tokenAmountETHER, _ := NewTokenAmount(tokenETHR, big.NewInt(100))
+		route, _ := NewRoute([]*Pair{pair_weth_0}, tokenETHER, nil)
 		trade, _ := NewTrade(route, tokenAmountETHER, constants.ExactInput)
 
 		// can be constructed with ETHER as input
 		{
-			expect := tokenETHR.Currency
+			expect := tokenETHER.Currency
 			output := trade.inputAmount.Currency
 			if !expect.Equals(output) {
 				t.Errorf("expect[%+v], but got[%+v]", expect, output)
@@ -63,10 +63,10 @@ func TestTrade(t *testing.T) {
 		}
 
 		// can be constructed with ETHER as input for exact output
-		route, _ = NewRoute([]*Pair{pair_weth_0}, tokenETHR, token0)
+		route, _ = NewRoute([]*Pair{pair_weth_0}, tokenETHER, token0)
 		trade, _ = NewTrade(route, tokenAmount_0_100, constants.ExactOutput)
 		{
-			expect := tokenETHR.Currency
+			expect := tokenETHER.Currency
 			output := trade.inputAmount.Currency
 			if !expect.Equals(output) {
 				t.Errorf("expect[%+v], but got[%+v]", expect, output)
@@ -80,7 +80,7 @@ func TestTrade(t *testing.T) {
 			}
 		}
 
-		route, _ = NewRoute([]*Pair{pair_weth_0}, token0, tokenETHR)
+		route, _ = NewRoute([]*Pair{pair_weth_0}, token0, tokenETHER)
 		// can be constructed with ETHER as output
 		trade, _ = NewTrade(route, tokenAmountETHER, constants.ExactOutput)
 		{
@@ -108,7 +108,7 @@ func TestTrade(t *testing.T) {
 			}
 		}
 		{
-			expect := tokenETHR.Currency
+			expect := tokenETHER.Currency
 			output := trade.outputAmount.Currency
 			if !expect.Equals(output) {
 				t.Errorf("expect[%+v], but got[%+v]", expect, output)
@@ -316,7 +316,7 @@ func TestTrade(t *testing.T) {
 		pairs = []*Pair{pair_0_1, pair_0_3, pair_1_3}
 		result, _ = BestTradeExactIn(pairs, tokenAmount, token2,
 			&BestTradeOptions{MaxNumResults: 1, MaxHops: 3}, nil, nil, nil)
-		// respects n
+		// no path
 		{
 			expect := 0
 			output := len(result)
@@ -324,5 +324,104 @@ func TestTrade(t *testing.T) {
 				t.Errorf("expect[%+v], but got[%+v]", expect, output)
 			}
 		}
+
+		pairs = []*Pair{pair_weth_0, pair_0_1, pair_0_3, pair_1_3}
+		result, _ = BestTradeExactIn(pairs, tokenAmountETHER, token3,
+			nil, nil, nil, nil)
+		// works for ETHER currency input
+		{
+			{
+				expect := 2
+				output := len(result)
+				if expect != output {
+					t.Fatalf("expect[%+v], but got[%+v]", expect, output)
+				}
+			}
+			{
+				var tests = []struct {
+					expect *Currency
+					output *Currency
+				}{
+					{_WETHCurrency, result[0].inputAmount.Currency},
+					{token3.Currency, result[0].outputAmount.Currency},
+					{_WETHCurrency, result[1].inputAmount.Currency},
+					{token3.Currency, result[1].outputAmount.Currency},
+				}
+				for i, test := range tests {
+					if !test.expect.Equals(test.output) {
+						t.Fatalf("test #%d: expect[%+v], but got[%+v]", i, test.expect, test.output)
+					}
+				}
+			}
+			{
+				var tests = []struct {
+					expect []*Token
+					output []*Token
+				}{
+					{[]*Token{tokenETHER, token0, token1, token3}, result[0].route.Path},
+					{[]*Token{tokenETHER, token0, token3}, result[1].route.Path},
+				}
+				for i, test := range tests {
+					if len(test.expect) != len(test.output) {
+						t.Fatalf("test #%d: expect[%+v], but got[%+v]", i, len(test.expect), (test.output))
+					}
+					for j := range test.expect {
+						if !test.expect[j].Equals(test.output[j]) {
+							t.Errorf("test #%d#%d: expect[%+v], but got[%+v]", i, j, test.expect[j], test.output[j])
+						}
+					}
+				}
+			}
+		}
+
+		tokenAmount, _ = NewTokenAmount(token3, big.NewInt(100))
+		result, _ = BestTradeExactIn(pairs, tokenAmount, tokenETHER,
+			nil, nil, nil, nil)
+		// works for ETHER currency output
+		{
+			{
+				expect := 2
+				output := len(result)
+				if expect != output {
+					t.Fatalf("expect[%+v], but got[%+v]", expect, output)
+				}
+			}
+			{
+				var tests = []struct {
+					expect *Currency
+					output *Currency
+				}{
+					{token3.Currency, result[0].inputAmount.Currency},
+					{_WETHCurrency, result[0].outputAmount.Currency},
+					{token3.Currency, result[1].inputAmount.Currency},
+					{_WETHCurrency, result[1].outputAmount.Currency},
+				}
+				for i, test := range tests {
+					if !test.expect.Equals(test.output) {
+						t.Fatalf("test #%d: expect[%+v], but got[%+v]", i, test.expect, test.output)
+					}
+				}
+			}
+			{
+				var tests = []struct {
+					expect []*Token
+					output []*Token
+				}{
+					{[]*Token{token3, token0, tokenETHER}, result[0].route.Path},
+					{[]*Token{token3, token1, token0, tokenETHER}, result[1].route.Path},
+				}
+				for i, test := range tests {
+					if len(test.expect) != len(test.output) {
+						t.Fatalf("test #%d: expect[%+v], but got[%+v]", i, len(test.expect), (test.output))
+					}
+					for j := range test.expect {
+						if !test.expect[j].Equals(test.output[j]) {
+							t.Errorf("test #%d#%d: expect[%+v], but got[%+v]", i, j, test.expect[j], test.output[j])
+						}
+					}
+				}
+			}
+		}
+
 	}
 }
